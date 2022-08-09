949743b9c4be421b834b8f42a4ff3239dc3b2a1b
ennajohn <88430741+ennajohn@users.noreply.github.com>
2022-08-09 08:35:45 +0530

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,22 @@
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: "png",
+    png_quality: 90
+});
+
+camera = document.getElementById("camera");
+Webcam.attach("#camera");
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("snap").innerHTML = "<img id='captured_image' src='"+data_uri+"'>";
+    });
+}
+
+console.log("ml5 version", ml5.version);
+Classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/9qdux8oqf/model.json",modelLoaded);
+
+function modelLoaded(){
+    console.log("modelLoaded");
+}
